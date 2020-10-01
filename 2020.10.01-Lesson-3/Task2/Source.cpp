734c9859c38a18e5d819c3fa19807ed58d0425a8
st021213 <st021213@student.spbu.ru>
2020-10-01 12:02:36 +0300

    Lesson 2 Task 3 finished

--- 2020.10.01-Lesson-3/Task2/Source.cpp
+++ 2020.10.01-Lesson-3/Task2/Source.cpp
@@ -3,5 +3,14 @@ using namespace std;
 
 int main()
 {
+	int* a = new int[5]{ 1, 2, 3, 4, 5 };
+	cout << "a\t=\t" << a << endl;
+	cout << "*a\t=\t" << *a << endl;
+	cout << "a[0]\t=\t" << a[0] << endl;
+	cout << "(a + 1)\t=\t" << a + 1 << endl;
+	cout << "*(a + 1)\t=\t" << *(a + 1) << endl;
+	cout << "a[1]\t=\t" << a[1] << endl;
+
+	delete[] a;
 	return EXIT_SUCCESS;
 }
